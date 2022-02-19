871bc54959f9e2f70aaeac670853e4f607a14171
Supasin Liulak <palsp2406@gmail.com>
2022-02-20 04:03:27 +0700

    scaffold candy machine

---
+++ tests/candy_machine.ts
@@ -0,0 +1,17 @@
+import * as anchor from '@project-serum/anchor'
+import { Program } from '@project-serum/anchor'
+import { CandyMachine } from '../target/types/candy_machine'
+
+describe('candy_machine', () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env())
+
+  // @ts-ignore
+  const program = anchor.workspace.CandyMachine as Program<CandyMachine>
+
+  it('Is initialized!', async () => {
+    const candyMachineId = 'CnkAuxyHvWj71Bvf7JGvTLG2UR94CA6HgNPuZfbJPkVA'
+    const state = await program.account.candyMachine.fetch(candyMachineId)
+    console.log(state)
+  })
+})
