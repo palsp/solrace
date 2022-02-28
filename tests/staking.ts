5297768935b209bedce35e93f7a4ab22ce1d4b48
Supasin Liulak <palsp2406@gmail.com>
2022-02-28 23:41:53 +0700

    init staking contract

--- tests/staking.ts
+++ tests/staking.ts
@@ -1,6 +1,7 @@
 import * as anchor from '@project-serum/anchor'
 import { Program } from '@project-serum/anchor'
 import { CandyMachine } from '../target/types/candy_machine'
+import { SolRaceStaking } from '../target/types/sol_race_staking'
 
 describe('candy_machine', () => {
   // Configure the client to use the local cluster.
@@ -8,11 +9,7 @@ describe('candy_machine', () => {
   anchor.setProvider(provider)
 
   // @ts-ignore
-  const program = anchor.workspace.CandyMachine as Program<CandyMachine>
+  const program = anchor.workspace.CandyMachine as Program<SolRaceStaking>
 
-  it('Is initialized!', async () => {
-    const candyMachineId = 'CnkAuxyHvWj71Bvf7JGvTLG2UR94CA6HgNPuZfbJPkVA'
-    const state = await program.account.candyMachine.fetch(candyMachineId)
-    console.log(state)
-  })
+  it('Is initialized!', async () => {})
 })
