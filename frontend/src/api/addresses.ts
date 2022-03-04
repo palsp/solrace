642a0a1a8ba8595f417b3fed6d0d3286aa9220d8
Supasin Liulak <palsp2406@gmail.com>
2022-03-05 01:09:10 +0700

    add idl and contract address

---
+++ frontend/src/api/addresses.ts
@@ -0,0 +1,25 @@
+import { PublicKey } from '@solana/web3.js'
+
+export const CANDY_MACHINE_PROGRAM = new PublicKey(
+  'cndy3Z4yapfJBmL3ShUp5exZKqR3z33thTzeNMm2gRZ',
+)
+
+export const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
+  'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s',
+)
+
+export const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new PublicKey(
+  'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL',
+)
+
+export const CIVIC = new PublicKey(
+  'gatem74V238djXdzWnJf94Wo1DcnuGkfijbf3AuBhfs',
+)
+
+export const SOL_RACE_STAKING_PROGRAM_ID = new PublicKey(
+  'u9vRU6Cb4kogHgVGofQKdSSsrCyhjHKKtQ7Uf7NX498',
+)
+
+export const SOLR_MINT_ADDRESS = new PublicKey(
+  '5j2kM5FbtZvi9nCadmU4pnheQaUqQm7QpTJSYKMUwzRp',
+)
