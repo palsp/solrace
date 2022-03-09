07491977a52119abda60f83f1858a49a8c72fc16
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-03-09 21:46:57 +0700

    feat: adjust Navbar

--- frontend/src/app/AppNav.tsx
+++ frontend/src/app/AppNav.tsx
@@ -1,14 +1,57 @@
-import styled from 'styled-components'
-import { useAuth } from '~/auth/hooks'
-import Link from 'next/link'
-import { AppLink, Row } from '~/ui'
-import ConnectWalletButton from '~/wallet/ConnectWalletButton'
-import Image from '~/ui/Image'
+import styled from "styled-components";
+import { useAuth } from "~/auth/hooks";
+import Link from "next/link";
+import { AppLink, Row } from "~/ui";
+import ConnectWalletButton from "~/wallet/ConnectWalletButton";
+import Image from "~/ui/Image";
+
+const AppNav = () => {
+  const { user } = useAuth();
+
+  return (
+    <NavContainer>
+      <UserSection>
+        <Link href="/" passHref>
+          <Image
+            src="/logo.png"
+            width="200px"
+            height="50px"
+            style={{ cursor: "pointer", paddingBottom: "1rem" }}
+          />
+        </Link>
+        <AppLink href="/garage">GARAGE</AppLink>
+        <AppLink href="/kart">KART</AppLink>
+        {!user ? (
+          <div>
+            <>
+              <AppLink href="/login">LOGIN</AppLink>
+              {" | "}
+              <AppLink href="/register">REGISTER</AppLink>
+            </>
+          </div>
+        ) : (
+          <div>
+            <AppLink href="/account">ACCOUNT</AppLink>
+          </div>
+        )}
+        <WrapperConnectWalletButton>
+          <ConnectWalletButton />
+        </WrapperConnectWalletButton>
+      </UserSection>
+    </NavContainer>
+  );
+};
+
+const WrapperConnectWalletButton = styled.div`
+  align-self: end;
+`;
 
 const UserSection = styled(Row)`
-  width: 40%;
+  width: 100%;
   justify-content: space-evenly;
-`
+  align-items: center;
+  gap: 5rem;
+`;
 
 const NavContainer = styled.div`
   position: fixed;
@@ -20,43 +63,6 @@ const NavContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   padding: 2em;
-`
+`;
 
-const AppNav = () => {
-  const { user } = useAuth()
-
-  return (
-    <NavContainer>
-      <Link href="/" passHref>
-        <Image
-          src="/logo.png"
-          width="200px"
-          height="50px"
-          style={{ cursor: 'pointer' }}
-        />
-      </Link>
-
-      <UserSection>
-        <AppLink href="/garage">GARAGE</AppLink>
-        <AppLink href="/kart">KART</AppLink>
-        {!user ? (
-          <div>
-            <>
-              <AppLink href="/login">LOGIN</AppLink>
-              {' | '}
-              <AppLink href="/register">REGISTER</AppLink>
-            </>
-          </div>
-        ) : (
-          <div>
-            <AppLink href="/account">ACCOUNT</AppLink>
-          </div>
-        )}
-
-        <ConnectWalletButton />
-      </UserSection>
-    </NavContainer>
-  )
-}
-
-export default AppNav
+export default AppNav;
