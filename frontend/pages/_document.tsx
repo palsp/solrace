8596a939075387d9fec181b2e7e0b01d0977767c
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-03-10 12:00:28 +0700

    fix: crossorigin

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -5,7 +5,11 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap"
           rel="stylesheet"
