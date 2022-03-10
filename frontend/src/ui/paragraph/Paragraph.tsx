6cba276c489fb7ce14bc0648b6162cc81b13a883
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-03-10 16:44:55 +0700

    feat: add Paragraph

---
+++ frontend/src/ui/paragraph/Paragraph.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Paragraph = styled.p`
+  font-size: 0.8rem;
+`;
+
+export const ParagraphItalic = styled(Paragraph)`
+  font-style: italic;
+`;
