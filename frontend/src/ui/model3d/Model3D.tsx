ce0559ec757f60d438cbb937a855d63fabc987d4
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-03-14 16:16:18 +0700

    feat: update 3D model with texture using gltf

--- frontend/src/ui/model3d/Model3D.tsx
+++ frontend/src/ui/model3d/Model3D.tsx
@@ -1,31 +1,78 @@
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { TGALoader } from "three/examples/jsm/loaders/TGALoader";
+
 import * as THREE from "three";
 import React, { Suspense } from "react";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Reflector, useTexture } from "@react-three/drei";
+
+function Ground(props: any) {
+  const [floor, normal] = useTexture([
+    "/SurfaceImperfections003_1K_var1.jpg",
+    "/marble-surface.jpeg",
+  ]);
+  return (
+    <Reflector resolution={256} args={[8, 8]} {...props}>
+      {(Material: any, props: any) => (
+        <Material
+          color="white"
+          metalness={0}
+          roughnessMap={floor}
+          normalMap={normal}
+          normalScale={[2, 2]}
+          {...props}
+        />
+      )}
+    </Reflector>
+  );
+}
 
 const Car = () => {
-  const fbx = useLoader(FBXLoader, "/Voyager.FBX");
-  return <primitive object={fbx} scale={0.02} />;
+  const fbx = useLoader(GLTFLoader, "/Cassini.gltf");
+  return (
+    <mesh>
+      <primitive
+        object={fbx.scene}
+        scale={1}
+        rotation={[0, 0, 0]}
+        position={[-6, -0.75, 0]}
+      />
+    </mesh>
+  );
 };
 
 const Model3D = () => {
   return (
-    <div>
-      <Canvas
-        camera={{
-          fov: 75,
-          near: 0.1,
-          far: 100,
-        }}
-      >
-        <Suspense fallback={null}>
-          <OrbitControls />
-          <pointLight intensity={3} />
-          <Car />
-        </Suspense>
-      </Canvas>
-    </div>
+    <Canvas
+      camera={{
+        fov: 75,
+        near: 0.01,
+        far: 1000,
+      }}
+      style={{
+        background: "var(--background-gradient-1)",
+      }}
+    >
+      <Suspense fallback={null}>
+        <OrbitControls
+          maxPolarAngle={Math.PI / 2}
+          maxDistance={5}
+          minDistance={3.5}
+          autoRotate
+        />
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Car />
+        <Ground
+          mirror={0.99}
+          blur={[500, 100]}
+          mixBlur={12}
+          mixStrength={1.5}
+          rotation={[-Math.PI / 2, 0, Math.PI / 2]}
+          position-y={-0.8}
+        />
+      </Suspense>
+    </Canvas>
   );
 };
 
