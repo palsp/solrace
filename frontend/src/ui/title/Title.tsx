472db17bf1d298d60373c282b73f8e773540a0c8
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-03-14 22:51:57 +0700

    feat: update Title adding fontStyle

--- frontend/src/ui/title/Title.tsx
+++ frontend/src/ui/title/Title.tsx
@@ -1,3 +1,13 @@
-const Title: React.FC = ({ children }) => <h1>{children}</h1>;
+import styled from "styled-components";
+interface Props {
+  fontStyle?: string;
+}
 
+const Title: React.FC<Props> = ({ children, fontStyle }) => {
+  return <StyledH1 fontStyle={fontStyle}>{children}</StyledH1>;
+};
+
+const StyledH1 = styled.h1`
+  font-style: ${(props: Props) => props.fontStyle};
+`;
 export default Title;
