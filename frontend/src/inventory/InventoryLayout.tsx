1d9730357948a5aa85954e231fb69f7f6205b556
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-03-15 16:26:57 +0700

    feat: add InventoryLayout

---
+++ frontend/src/inventory/InventoryLayout.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import AppLayout from "~/app/AppLayout";
+
+interface Props {
+  direction: string;
+  cards?: JSX.Element | undefined;
+}
+const InventoryLayout: React.FC<Props> = ({ direction, children, cards }) => {
+  return (
+    <AppLayout>
+      <InventoryLayoutContainer direction={direction}>
+        <WrapperDescription>{children}</WrapperDescription>
+        <WrapperCards>{cards}</WrapperCards>
+      </InventoryLayoutContainer>
+    </AppLayout>
+  );
+};
+
+const InventoryLayoutContainer = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: ${(props: Props) => props.direction};
+  gap: 2rem;
+`;
+
+const WrapperDescription = styled.div`
+  flex: 3;
+  margin: 2rem 0;
+`;
+
+const WrapperCards = styled.div`
+  flex: 7;
+  margin: 2rem 0;
+`;
+export default InventoryLayout;
