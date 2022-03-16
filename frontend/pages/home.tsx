7090b06caed6fb4d583cb3defa8893e173ec31d3
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-03-16 13:28:35 +0700

    refactor: move HomeNav directory

--- frontend/pages/home.tsx
+++ frontend/pages/home.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import HorizontalScroll from "react-scroll-horizontal";
 import styled from "styled-components";
-import HomeNav from "~/app/HomeNav";
-import { Trailer, GameDetail, GamePlay, Roadmap, Team } from "~/home";
+import { Trailer, GameDetail, GamePlay, Roadmap, Team, HomeNav } from "~/home";
 
 const home = () => {
   return (
-    <div style={{ width: `100vw`, height: `100%`, overflow: "auto" }}>
-      <HorizontalScroll>
-        <HomeNav />
-        <Trailer />
-        <GameDetail />
-        <GamePlay />
-        <Roadmap />
-        <Team />
-      </HorizontalScroll>
-    </div>
+    // <div style={{ width: `100vw`, height: `100%`, overflow: "auto" }}>
+    <HorizontalScroll>
+      <HomeNav />
+      <Trailer />
+      <GameDetail />
+      <GamePlay />
+      <Roadmap />
+      <Team />
+    </HorizontalScroll>
+    // </div>
   );
 };
 
--- frontend/src/home/index.ts
+++ frontend/src/home/index.ts
@@ -3,3 +3,4 @@ export { default as GameDetail } from "./GameDetail";
 export { default as GamePlay } from "./GamePlay";
 export { default as Roadmap } from "./Roadmap";
 export { default as Team } from "./Team";
+export { default as HomeNav } from "./HomeNav";
