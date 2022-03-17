a830956ae3423bc94414b97ef0d78607757e421c
Supasin Liulak <palsp2406@gmail.com>
2022-03-17 23:05:09 +0700

    update cm address

--- frontend/src/api/solana/addresses.ts
+++ frontend/src/api/solana/addresses.ts
@@ -1,43 +1,43 @@
-import { PublicKey } from '@solana/web3.js'
+import { PublicKey } from "@solana/web3.js";
 
 export const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
-  'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s',
-)
+  "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+);
 
 export const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new PublicKey(
-  'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL',
-)
+  "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"
+);
 
 export const CIVIC = new PublicKey(
-  'gatem74V238djXdzWnJf94Wo1DcnuGkfijbf3AuBhfs',
-)
+  "gatem74V238djXdzWnJf94Wo1DcnuGkfijbf3AuBhfs"
+);
 
 export const CANDY_MACHINE_PROGRAM = new PublicKey(
-  'cndy3Z4yapfJBmL3ShUp5exZKqR3z33thTzeNMm2gRZ',
-)
+  "cndy3Z4yapfJBmL3ShUp5exZKqR3z33thTzeNMm2gRZ"
+);
 
 export const SOL_RACE_CORE_PROGRAM_ID = new PublicKey(
-  '4tcD8pgdaSn4yzWQybKuKzSXpW3uoDugFY6owNUhhexd',
-)
+  "4tcD8pgdaSn4yzWQybKuKzSXpW3uoDugFY6owNUhhexd"
+);
 
 export const SOLR_MINT_ADDRESS = new PublicKey(
-  'HQ7PKsKfk9EPtmHercrWs9ymUZqut7tS1e9ktpb8qd5n',
-)
+  "HQ7PKsKfk9EPtmHercrWs9ymUZqut7tS1e9ktpb8qd5n"
+);
 
 export const GARAGE_CREATOR = new PublicKey(
-  'AkuCDDdZhWi6GyuPLeVi1FVtajZ6Bbyj9hJEgDYRcfku',
-)
+  "BrdAi9KnJrMMjjRAMBr4QLft2PpEyR3L6wp2ruceovhS"
+);
 
 export const KART_CREATOR = new PublicKey(
-  'GKouHKo891dGKvrWGi3Ld81ESaRrcUWpgqzhf5N4UZJH',
-)
+  "7uGWKJKxKvxE1Hx5G4L9WMoUJyXcYjoqtpRg27kErVZk"
+);
 
 export const GARAGE_CM_ID = new PublicKey(
-  '7ZMyBKRwgGomnwAUkrZrAsnHiKyYqgvyBgid4pzmPWXr',
-)
+  "FqCvWhNQr6EZuK656hejwBM22DRfRSGah4zKejGDcftV"
+);
 
 export const KART_CM_ID = new PublicKey(
-  'CiC9WDoGhLFEXgoCPLirhszNXjJJDCzUESQWnPdFucZ9',
-)
+  "4CyKQTY6nRtTBYjQF6kgmNT6etVBHqcpskx75Cc4JRrA"
+);
 
-export const VERIFY_NFT_PROGRAM = 'HuP1D9qVpK61WJc5WoCxkYGGbY1s8wGPXaDM2Rq6pBN'
+export const VERIFY_NFT_PROGRAM = "HuP1D9qVpK61WJc5WoCxkYGGbY1s8wGPXaDM2Rq6pBN";
